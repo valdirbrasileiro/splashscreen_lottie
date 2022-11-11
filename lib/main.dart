8bc6f6780793b675c098609d0f8d8de01a066588
Valdir Brasileiro <valdirbrasileiro@MacBook-Pro-de-Valdir.local>
2022-11-10 21:47:51 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen_lottie/layout/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Lottie.asset("assets/99069-flutter-logo.json");
+  }
+}
